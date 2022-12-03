f80e5ed1e30ae76da75220af9c488c1fb902099d
ddd-fx <d.khoroshkin@gmail.com>
2022-12-03 18:00:39 +0600

    update api url

--- src/types/types.ts
+++ src/types/types.ts
@@ -162,9 +162,9 @@ export type SortArgsType = {
 };
 
 export enum RequestURL {
-  garage = 'http://127.0.0.1:3000/garage/',
-  engine = 'http://127.0.0.1:3000/engine',
-  winners = 'http://127.0.0.1:3000/winners/',
+  garage = 'https://teal-scorpion-tie.cyclic.app/garage/',
+  engine = 'https://teal-scorpion-tie.cyclic.app/engine',
+  winners = 'https://teal-scorpion-tie.cyclic.app/winners/',
 }
 
 export type WinnersQueryType = Pick<WinnersStateType, 'winnersPage' | 'sortBy' | 'sortOrder'>;
